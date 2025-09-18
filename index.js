871a3bcb7b7da960d2219c6ee4bb4a5c71c63137
RiiOcha1 <nerome.riko@is.ocha.ac.jp>
2025-09-18 05:28:31 +0000

    add user certification

---
+++ index.js
@@ -0,0 +1,24 @@
+// index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_Mw4V1Mnp8",
+  client_id: "4vu82ghmfh5tk5adijse0dbf2i",
+  redirect_uri: "https://main.d2s921w9x0k31c.amplifyapp.com/",
+  response_type: "code",
+  scope: "email openid phone",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// wrap the application with AuthProvider
+root.render(
+  <React.StrictMode>
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
